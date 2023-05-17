cff354af457711dc51e4aacfe1f31a4ce3bcefa9
Rupesh singh <70444395+rupesh-hash@users.noreply.github.com>
2023-05-17 11:14:58 +0530

    Update Footer.js
    rupesh footer

--- src/Footer.js
+++ src/Footer.js
@@ -4,7 +4,7 @@ import React from 'react';
 const Footer =()=>{
     return(<>
     <div className="footer__div bg-dark">
-    <p className="text-center text-capitalize text-white bg-dark"> © Copyright Saif Khan 2020 </p>
+    <p className="text-center text-capitalize text-white bg-dark"> © Copyright Rupesh Singh 2023 </p>
     </div>
     
     </>)
